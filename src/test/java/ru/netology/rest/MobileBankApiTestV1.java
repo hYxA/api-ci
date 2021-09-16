fe0bc19578fbc348ed7df204ddbac0a125438ae9
hYxA <zybu@live.ru>
2021-09-16 16:16:44 +0300

    .statusCode(201);

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -16,6 +16,6 @@ public class MobileBankApiTestV1 {
                 .get("/demo/accounts")
 //        Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(201);
     }
 }
