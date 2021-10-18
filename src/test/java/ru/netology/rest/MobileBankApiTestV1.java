ae85482c7dfc124261127fd453d42c27dae4ccc1
hYxA <zybu@live.ru>
2021-10-18 16:27:50 +0300

    hasSize

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.hasSize;
 
 public class MobileBankApiTestV1 {
     @Test
@@ -19,6 +20,7 @@ public class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .header("Content-Type", "application/json; charset=UTF-8");
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body("", hasSize(3));
     }
 }
