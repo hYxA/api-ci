b423d35ae706901a854d7d866c6be29717d65861
hYxA <zybu@live.ru>
2021-10-18 15:06:55 +0300

    create branch

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,6 +19,6 @@ public class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .header("currency", "RUB");
+                .header("currency", "RUr");
     }
 }
