fce5ceeddaf27d073043d4a85a47e2662a202793
hYxA <zybu@live.ru>
2021-10-31 12:56:43 +0300

    Added new tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class MobileBankApiTestV1 {
     @Test
@@ -19,5 +21,51 @@ public class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+    @Test
+    void shouldMatchUSD() {
+//        Given - When - Then
+//        Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+//        Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+//        Проверки
+                .then()
+                .statusCode(200)
+                .body("[1].currency", equalTo("USD"))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+    @Test
+    void shouldMatchHeader() {
+//        Given - When - Then
+//        Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+//        Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+//        Проверки
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+    @Test
+    void shouldMatchHasSize() {
+//        Given - When - Then
+//        Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+//        Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+//        Проверки
+                .then()
+                .body("", hasSize(3));
     }
 }
